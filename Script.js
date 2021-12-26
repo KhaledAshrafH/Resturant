f0ad85a4d853f7c0f8996eb26f7d21b896d8b829
Khaled Ashraf <83093446+KhaledAshraf74@users.noreply.github.com>
2021-12-27 01:09:04 +0200

    Resturant

---
+++ Script.js
@@ -0,0 +1,10 @@
+function loadText() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "learn.html", true);
+    xhttp.send();
+  }
